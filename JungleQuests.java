89731e022dfd4dfe17b4255a13df76e403376ae7
C Namith Jain <cnamithjainbsc24@rvu.edu.in>
2025-11-08 10:39:33 +0530

    Refactor jungle quest choices and outcomes

--- JungleQuests.java
+++ JungleQuests.java
@@ -31,10 +31,12 @@
 
  } else if (choice1 == 2) {
             System.out.println("\nYou reach a waterfall.");
-             System.out.println("1. Go behind the waterfall ");
+            System.out.println("1. Go behind the waterfall ");
             System.out.println("2. Follow the river downstream ");
             System.out.println("3. Climb the cliff ");
             System.out.print("Choose (1, 2 or 3): ");
+            int choice3 = sc.nextInt();
+
 
 
 
