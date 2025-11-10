02f4f0680d7a15f3f859b49da5880760485ab43e
C Namith Jain <cnamithjainbsc24@rvu.edu.in>
2025-11-10 14:23:02 +0530

    Refactor JungleQuests for additional choices and paths

--- JungleQuests.java
+++ JungleQuests.java
@@ -67,7 +67,66 @@ if (choice2 == 1) {
             }
         }
 
+//namith
 
+            else if (choice1 == 2) {
+            System.out.println("\nYou reach a waterfall.");
+            System.out.println("1. Go behind the waterfall ");
+            System.out.println("2. Follow the river downstream ");
+            System.out.println("3. Climb the cliff ");
+            System.out.print("Choose (1, 2 or 3): ");
+            int choice3 = sc.nextInt();
+
+            if (choice3 == 1) {
+                System.out.println("\nYou find a cave with treasure.");
+                System.out.println("1. Take the treasure ");
+                System.out.println("2. Leave it ");
+                System.out.print("Choose (1 or 2): ");
+                int choice4 = sc.nextInt();
+
+                if (choice4 == 1) {
+                    System.out.println("\nThe cave collapses.");
+                    System.out.println(" GAME OVER.");
+                } else {
+                    System.out.println("\nA secret exit opens!");
+                    System.out.println(" YOU WIN!");
+                }
+
+            } else if (choice3 == 2) {
+                System.out.println("\nYou see a raft in a swamp.");
+                System.out.println("1. Use the raft ");
+                System.out.println("2. Swim ");
+                System.out.print("Choose (1 or 2): ");
+                int choice4 = sc.nextInt();
+
+                if (choice4 == 1) {
+                    System.out.println("\nYou escape using the raft!");
+                    System.out.println(" YOU WIN!");
+                } else {
+                    System.out.println("\nSnakes attack you.");
+                    System.out.println(" GAME OVER.");
+                }
+
+            } else if (choice3 == 3) {
+                System.out.println("\nYou find a stone tablet.");
+                System.out.println("1. Touch it ");
+                System.out.println("2. Ignore it ");
+                System.out.print("Choose (1 or 2): ");
+                int choice4 = sc.nextInt();
+
+                if (choice4 == 1) {
+                    System.out.println("\nYou become the guardian of the jungle.");
+                    System.out.println(" YOU WIN!");
+                } else {
+                    System.out.println("\nThe ground collapses. You fall.");
+                    System.out.println(" GAME OVER.");
+                }
+
+            } else {
+                System.out.println("\nInvalid choice!");
+                System.out.println(" GAME OVER.");
+            }
+        }
 
 
         
@@ -82,6 +141,7 @@ else {
         sc.close();
     }
 }
+
 
 
 
