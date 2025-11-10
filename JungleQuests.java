32c8e46b1e2059d3d544bc912de999960ea7d5f0
C Namith Jain <cnamithjainbsc24@rvu.edu.in>
2025-11-10 10:51:34 +0530

    Add choices for waterfall encounter in JungleQuests

--- JungleQuests.java
+++ JungleQuests.java
@@ -7,5 +7,12 @@ else if (choice1 == 2) {
             System.out.print("Choose (1, 2 or 3): ");
             int choice3 = sc.nextInt();
 
+if (choice3 == 1) {
+                System.out.println("\nYou find a cave with treasure.");
+                System.out.println("1. Take the treasure ");
+                System.out.println("2. Leave it ");
+                System.out.print("Choose (1 or 2): ");
+                int choice4 = sc.nextInt();
+
 
 
